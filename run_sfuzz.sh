ff78e5271bc952495d829b57161435327f736045
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-24 15:22:22 +0800

    add a script to run all sfuzz experiments

---
+++ run_sfuzz.sh
@@ -0,0 +1,7 @@
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/block_dependency/ 3600 20
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/integer_overflows/ 3600 20
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/locking_ether/ 3600 20
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/transaction_order_dependency/ 3600 20
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/reentrancy 3600 20
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/unhandled_exception/ 3600 20
+python run_sfuzz_experiment.py benchmarks/confuzzius_curated/unsafe_delegatecall/ 3600 20
