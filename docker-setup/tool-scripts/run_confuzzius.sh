98edc32feb0f193bfdb637548260171d6211b6fd
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-27 11:30:38 +0800

    add script to run confuzzius

---
+++ docker-setup/tool-scripts/run_confuzzius.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Arg1 : Time limit
+# Arg2 : Source file
+
+TOOLDIR=/home/test/tools/confuzzius
+WORKDIR=/home/test/mythril-workspace
+
+FILENAME="$2"
+VERSION=$(python3 /home/test/scripts/get_solc_version.py "$FILENAME")
+solc-select install $VERSION
+solc-select use $VERSION
+
+# Set up workdir
+mkdir -p $WORKDIR
+# Set up environment
+mkdir -p $WORKDIR/output
+mkdir -p $WORKDIR/output/bugs
+touch $WORKDIR/output/log.txt
+# Run mythril
+
+python3 $TOOLDIR/ConFuzzius/fuzzer/main.py -s $2 --solc "v$VERSION" -t $1 > \
+                      $WORKDIR/output/stdout.txt 2>&1
+
+cp -r $WORKDIR/output/ /home/test/
